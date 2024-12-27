e711cad9721fddb2c75a6238d3b9bf1900e497d5
Ethan Johnson <37124195+Asaayu@users.noreply.github.com>
2024-12-27 22:56:26 +1300

    chore: remove un-needed file

--- full_release.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-msbuild extension/IntegratedVoiceControlSystem.sln /p:Configuration=Release /p:Platform=x64
-msbuild extension/IntegratedVoiceControlSystem.sln /p:Configuration=Release /p:Platform=x86
-hemtt build
-pause
